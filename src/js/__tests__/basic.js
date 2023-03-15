21dbfb43733c089df07d3e66bee9c9514ee67d13
Alexander B <bas2784@gmail.com>
2023-03-15 12:03:55 +0300

    configs, appveyor, readme

--- src/js/__tests__/basic.js
+++ src/js/__tests__/basic.js
@@ -1,18 +1,19 @@
-import sortHealth from "../filterHealth";
-
+import sortHealth from '../filterHealth';
 
 const ArrayCharacters = [
-	{name: 'мечник', health: 10},
-	{name: 'маг', health: 100},
-	{name: 'лучник', health: 80},
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+  { name: 'викинг', health: 10 },
 ];
 
 const res = [
-	{ name: 'маг', health: 100 },
-	{ name: 'лучник', health: 80 },
-	{ name: 'мечник', health: 10 }
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+  { name: 'мечник', health: 10 },
+  { name: 'викинг', health: 10 },
 ];
 
 test('sorting health value', () => {
-	expect(sortHealth(ArrayCharacters).toBe(res))
-})
+  expect(sortHealth(ArrayCharacters)).toEqual(res);
+});
--- src/js/filterHealth.js
+++ src/js/filterHealth.js
@@ -1,9 +1,12 @@
-function compare(a, b) {
-	if (a.health > b.health) return -1;
-	if (a.health === b.health) return 0;
-	if (a.health < b.health) return 1;
+function compareValue(a, b) {
+  if (a.health > b.health) {
+    return -1;
+  } if (a.health === b.health) {
+    return 0;
+  }
+  return 1;
 }
 
 export default function sortHealth(arrayCharacters) {
-	return arrayCharacters.sort(compare)
+  return arrayCharacters.sort(compareValue);
 }
